7ab65d70e8ac6e900790cdd72cf72bae27850a21
Ing. Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2024-01-06 18:56:30 -0500

    Update numero_de_caracter.py

--- numero_de_caracter.py
+++ numero_de_caracter.py
@@ -1,10 +1,18 @@
-"""La función recorre una lista de caracteres y para cada carácter en la lista, 
-encuentra su número correspondiente en el sistema Unicode utilizando la función
-ord(). Luego, imprime cada carácter junto con su número Unicode."""
-
 caracteres = ['z', 'G', '0', '1', '2', '$', '\\']
 
 for caracter in caracteres:
+    """
+    Itera a través de la lista de caracteres y muestra su respectivo número Unicode.
+
+    Parámetros:
+    caracter (str): El carácter actual en la lista.
+
+    Proceso:
+    Utiliza la función ord() para encontrar el número correspondiente en el sistema Unicode para cada carácter en la lista.
+    Luego, imprime el carácter junto con su número Unicode.
+
+    Retorna:
+    None
+    """
     numero_unicode = ord(caracter)
     print(f"El número Unicode para '{caracter}' es: {numero_unicode}")
-
